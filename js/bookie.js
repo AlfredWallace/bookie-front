99722f727e9ce7761f007accbd53ee867630541a
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-05-05 16:58:19 +0200

    Creating a Vue component for the login form

--- js/bookie.js
+++ js/bookie.js
@@ -1,9 +1,25 @@
-new Vue({
-    el: '#login_form',
-    data: {
-        userLogin:'',
-        userPassword:''
+Vue.component('login-form', {
+    data: () => {
+        return {
+            userLogin:'',
+            userPassword:''
+        };
     },
+    template: `
+        <div id="login_form" class="row h-100 align-items-center justify-content-around">
+            <div class="col col-md-6 col-lg-4">
+                <div class="form-group">
+                    <input type="text" v-model="userLogin" id="userLogin" placeholder="Username"
+                           class="form-control form-control-lg">
+                </div>
+                <div class="form-group">
+                    <input type="password" v-model="userPassword" id="userPassword" placeholder="Password"
+                           class="form-control form-control-lg">
+                </div>
+                <button @click="getApiToken" class="btn btn-lg btn-info w-100">Login</button>
+            </div>
+        </div>
+    `,
     methods: {
         getApiToken: function() {
             axios.post('http://local.bookie-api.alfred-wallace.com/login_check', {
@@ -15,4 +31,8 @@ new Vue({
             });
         }
     }
+});
+
+new Vue({
+    el: '#main-container'
 });
