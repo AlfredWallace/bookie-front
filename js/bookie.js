5a7435ab575446c5895be8c71737193b58d005ee
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-05-11 21:14:08 +0200

    Displaying the date nicely

--- js/bookie.js
+++ js/bookie.js
@@ -44,13 +44,17 @@ Vue.component('login-form', {
 Vue.component('match-list', {
     data: function() {
         return {
-            matches: null
+            matches: null,
+            months: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+                'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+            days: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
         };
     },
     props: ['apiBaseUrl', 'token'],
     template: `
         <div class="row bk-header-shift">
-            <match v-for="match in matches" :match="match" :key="match.id"></match>
+            <match v-for="match in matches" :match="match" :key="match.id" 
+                :months="months" :days="days"></match>
         </div>
     `,
     methods: {
@@ -78,11 +82,23 @@ Vue.component('match', {
             flagsUrl: 'https://fsprdcdnpublic.azureedge.net/global-pictures/flags-fwc2018-4/'
         };
     },
-    props: ['match'],
+    props: ['match', 'months', 'days'],
+    computed: {
+        formatedKickOff: function () {
+            let kickOffDate = new Date(this.match.kick_off);
+            let date = kickOffDate.getDate();
+            let dateText = date === 1 ? date + 'er' : date;
+            let day = this.days[kickOffDate.getDay()];
+            let month = this.months[kickOffDate.getMonth()];
+            let minutes = kickOffDate.getMinutes() < 10 ? '0' + kickOffDate.getMinutes() : kickOffDate.getMinutes();
+            let hours = kickOffDate.getHours() < 10 ? '0' + kickOffDate.getHours() : kickOffDate.getHours();
+            return day + ' ' + dateText + ' ' + month + ' à ' + hours + 'h' + minutes;
+        }
+    },
     template: `
          <div class="col-12 col-lg-6 mb-4">
             <div class="card border-info">
-                <div class="card-header text-white bg-info">{{ match.kick_off }}</div>
+                <div class="card-header text-center text-white bg-info">{{ formatedKickOff }}</div>
                 <div class="card-body text-info bk-match-card-content">
                     <div class="container-fluid">
                     <div class="row">
