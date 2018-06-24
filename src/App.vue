0ce8bbdc801db7e317bc67a4dcdb1598bba65e35
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-06-24 22:54:11 +0200

    [IMORTANT COMMIT] replacing the 'page' custom functionnality with the vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,32 @@
     <div class="h-100">
         <nav class="navbar navbar-expand bg-info navbar-dark fixed-top bk-header">
             <div class="container">
-                <div v-if="loggedIn" class="navbar-nav w-100 d-flex">
+                <div v-if="loggedIn === true" class="navbar-nav w-100 d-flex">
 
-                    <a class="nav-item nav-link mr-auto" href="#" @click="logOut">
+                    <a class="nav-item nav-link mr-auto" @click="logOut">
                         <font-awesome-icon icon="user-slash" class="fa-lg"></font-awesome-icon>
                         <span :class="responsiveDisplay">Déconnexion</span>
                     </a>
-                    <a v-if="isAdmin" class="nav-item nav-link" :class="{ active: page === 'admin' }" href="#"
-                       @click="showAdmin">
+
+                    <router-link v-if="isAdmin" class="nav-item nav-link" to="/admin">
                         <font-awesome-icon icon="unlock" class="fa-lg"></font-awesome-icon>
                         <span :class="responsiveDisplay">Admin</span>
-                    </a>
+                    </router-link>
 
-                    <a class="nav-item nav-link" :class="{ active: page === 'match-bet-list' }" href="#"
-                       @click="showMatchBetList">
+                    <router-link class="nav-item nav-link" to="/pronostics">
                         <font-awesome-icon icon="futbol" class="fa-lg"></font-awesome-icon>
                         <span :class="responsiveDisplay">Matchs</span>
-                    </a>
+                    </router-link>
 
-                    <a class="nav-item nav-link" :class="{ active: page === 'rank-list' }" href="#"
-                       @click="showRankList">
+                    <router-link class="nav-item nav-link" to="/classement">
                         <font-awesome-icon icon="list-ol" class="fa-lg"></font-awesome-icon>
                         <span :class="responsiveDisplay">Classement</span>
-                    </a>
+                    </router-link>
 
-                    <a class="nav-item nav-link" :class="{ active: page === 'match-history-list' }" href="#"
-                       @click="showMatchHistoryList">
+                    <router-link class="nav-item nav-link" to="/historique">
                         <font-awesome-icon icon="history" class="fa-lg"></font-awesome-icon>
                         <span :class="responsiveDisplay">Historique</span>
-                    </a>
+                    </router-link>
 
                 </div>
                 <span v-else class="navbar-brand w-100 text-center">
@@ -39,26 +36,19 @@
             </div>
         </nav>
         <div id="main-container" class="container h-100">
-            <match-bet-list v-if="loggedIn && page === 'match-bet-list'" :api-base-url="apiBaseUrl" :token="token"
-                            :user-id="userId"></match-bet-list>
 
-            <match-history-list v-if="loggedIn && page === 'match-history-list'" :api-base-url="apiBaseUrl" :token="token"
-                                :user-id="userId"></match-history-list>
+            <router-view v-if="loggedIn === true" :api-base-url="apiBaseUrl" :token="token" :user-id="userId">
+            </router-view>
 
-            <rank-list v-else-if="loggedIn && page === 'rank-list'" :api-base-url="apiBaseUrl" :token="token"
-                       :user-id="userId"></rank-list>
-
-            <admin v-else-if="loggedIn && page === 'admin'" :api-base-url="apiBaseUrl" :token="token"
-                   :user-id="userId"></admin>
-
-            <login-form v-else-if="loggedIn !== true" :api-base-url="apiBaseUrl"></login-form>
+            <login-form v-else :api-base-url="apiBaseUrl"></login-form>
 
             <notifications position="top left">
                 <template slot="body" slot-scope="props">
                     <div class="bk-notif rounded d-flex justify-content-between align-items-center border"
                          :class="props.item.type">
                         <div v-html="props.item.text"></div>
-                        <font-awesome-icon icon="times-circle" @click="props.close" class="bk-cursor"></font-awesome-icon>
+                        <font-awesome-icon icon="times-circle" @click="props.close" class="bk-cursor">
+                        </font-awesome-icon>
                     </div>
                 </template>
             </notifications>
@@ -68,14 +58,10 @@
 
 <script>
     import LoginForm from './components/LoginForm';
-    import MatchBetList from './components/MatchBetList';
-    import MatchHistoryList from './components/MatchHistoryList';
-    import RankList from './components/RankList';
-    import Admin from './components/Admin';
 
     export default {
         name: "App",
-        components: {LoginForm, MatchBetList, MatchHistoryList, RankList, Admin},
+        components: {LoginForm},
         data () {
             return {
                 loggedIn: false,
@@ -83,7 +69,6 @@
                 token: null,
                 payload: null,
                 userId: null,
-                page: null,
                 isAdmin: false,
                 responsiveDisplay: 'd-none d-lg-inline'
             };
@@ -103,25 +88,6 @@
             })
         },
         methods: {
-            showRankList () {
-                this.page = 'rank-list';
-                this.setPageCookie();
-            },
-            showMatchBetList () {
-                this.page = 'match-bet-list';
-                this.setPageCookie();
-            },
-            showMatchHistoryList () {
-                this.page = 'match-history-list';
-                this.setPageCookie();
-            },
-            showAdmin () {
-                this.page = 'admin';
-                this.setPageCookie();
-            },
-            setPageCookie () {
-                this.$cookie.set('landing_page', this.page, { expires: 7});
-            },
             isTokenExpired () {
                 if (this.payload !== null && this.payload.hasOwnProperty('exp')) {
                     let currentTimestamp = (new Date()).getTime() / 1000;
@@ -137,14 +103,6 @@
                 if (!this.isTokenExpired()) {
                     this.loggedIn = true;
 
-                    let pageCookie = this.$cookie.get('landing_page');
-                    if (pageCookie !== null) {
-                        this.page = pageCookie;
-                    } else {
-                        this.page = 'match-bet-list';
-                        this.setPageCookie();
-                    }
-
                     if (this.payload.hasOwnProperty('userId')) {
                         this.userId = this.payload.userId;
                     }
@@ -159,7 +117,6 @@
                 this.token = null;
                 this.payload = null;
                 this.userId = null;
-                this.page = null;
                 this.isAdmin = false;
             }
         },
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 
 import axios from 'axios';
@@ -20,9 +21,28 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(Notifications);
 Vue.use(Cookie);
 Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
+
+import MatchBetList from './components/MatchBetList';
+import MatchHistoryList from './components/MatchHistoryList';
+import RankList from './components/RankList';
+import Admin from './components/Admin';
+
+
+const routes = [
+    { path: '/pronostics', component: MatchBetList, },
+    { path: '/historique', component: MatchHistoryList, },
+    { path: '/classement', component: RankList, },
+    { path: '/admin', component: Admin, },
+];
+const router = new VueRouter({
+    routes: routes,
+    mode: 'history',
+});
 
 new Vue({
     el: '#app',
+    router,
     template: '<App/>',
     components: { App },
 });
